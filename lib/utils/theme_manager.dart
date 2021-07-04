571bcceb6729b49dd23797e1fcac2cd2e92dc43e
Nikhil Gupta <34468304+niklodeon@users.noreply.github.com>
2021-07-04 15:18:48 +0200

    Update theme_manager.dart

--- lib/utils/theme_manager.dart
+++ lib/utils/theme_manager.dart
@@ -3,14 +3,12 @@ import 'package:flutter101/constants/themes.dart';
 
 class ThemeChanger extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.light;
-  ThemeData _lightTheme = lightTheme;
-  ThemeData _darkTheme = darkTheme;
 
   ThemeMode get themeMode => _themeMode;
 
-  ThemeData get lightTheme => _lightTheme;
+  ThemeData get lightTheme => lightTheme;
 
-  ThemeData get darkTheme => _darkTheme;
+  ThemeData get darkTheme => darkTheme;
 
   toggleTheme(){
     _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
