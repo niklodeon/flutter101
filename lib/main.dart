9b26cb83d2d760051ecb8e0711e076dcc6f73cc4
Nikhil Gupta <34468304+niklodeon@users.noreply.github.com>
2021-07-04 11:09:27 +0200

    Update main.dart
    Introduced theme notifier and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,33 @@
+import 'package:flutter101/utils/theme_notifier.dart';
+import 'package:flutter101/constants/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(Flutter101App());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]).then((_) {
+    SharedPreferences.getInstance().then((prefs) {
+      var darkModeOn = prefs.getBool('darkMode') ?? true;
+      runApp(
+        ChangeNotifierProvider<ThemeNotifier>(
+          builder: (_) => ThemeNotifier(darkModeOn ? darkTheme : lightTheme),
+          child: Flutter101App(),
+        ),
+      );
+    });
+  });
 }
 
 class Flutter101App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
-      title: 'Flutter 10@',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Flutter 101',
+      theme: themeNotifier.getTheme(),
       home: HomePage(title: 'Flutter 101 Home Page'),
     );
   }
