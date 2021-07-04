9fab3603dc18c167b851b0a6c427524a1f23e0e1
Nikhil Gupta <34468304+niklodeon@users.noreply.github.com>
2021-07-04 21:09:27 +0200

    Update themes.dart

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter101/constants/themes.dart';
 
-CustomTheme currentTheme = CustomTheme();
-
 class CustomTheme with ChangeNotifier {
   static bool _isDarkTheme = false;
   ThemeMode get currentTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
