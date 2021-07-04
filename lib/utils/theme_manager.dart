82e9019be5d88baab700f91938119d3498193747
Nikhil Gupta <34468304+niklodeon@users.noreply.github.com>
2021-07-04 14:36:27 +0200

    Update and rename theme_provider.dart to theme_manager.dart

--- lib/utils/theme_manager.dart
+++ lib/utils/theme_manager.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter101/constants/themes.dart';
 
-class ThemeNotifier extends ChangeNotifier {
+class ThemeChanger extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.dark;
 
   ThemeMode get themeMode => _themeMode;
 
-  ThemeData get getTheme => _themeMode == ThemeMode.light ? lightTheme : darkTheme;
+  ThemeData get lightTheme => lightTheme;
+
+  ThemeData get darkTheme => darkTheme;
 
   toggleTheme(){
-    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.light : ThemeMode.dark;
+    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
     notifyListeners();
   } 
 }
