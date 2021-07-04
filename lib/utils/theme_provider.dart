7616d10f1ea4435544a4653c83800de99f3c8972
Nikhil Gupta <34468304+niklodeon@users.noreply.github.com>
2021-07-04 13:06:00 +0200

    Update and rename theme_notifier.dart to theme_provider.dart

---
+++ lib/utils/theme_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter101/constants/themes.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  final String key = "theme";
+  SharedPreferences _pref;
+  bool _darkTheme;
+
+  ThemeNotifier() {
+    _darkTheme = true;
+    _loadFromPrefs();
+  }
+
+  ThemeData getTheme(){
+    _darkTheme ? darkTheme : lightTheme;
+  }
+
+  toggleTheme(){
+    _darkTheme = !_darkTheme;
+    _saveToPrefs();
+    notifyListeners();
+  }
+
+  _initPrefs() async {
+    if(_pref == null)
+      _pref  = await SharedPreferences.getInstance();
+  }
+
+  _loadFromPrefs() async {
+      await _initPrefs();
+      _darkTheme = _pref.getBool(key) ?? true;
+      notifyListeners();
+  }
+  
+  _saveToPrefs() async {
+    await _initPrefs();
+    _pref.setBool(key, _darkTheme);
+  }
+}
